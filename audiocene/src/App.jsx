e475e41379f9473ca354a6c3ae1f5d1ac2213626
Andy Soloman <andyjsoloman@gmail.com>
2024-05-05 19:55:29 -0700

    login page

--- audiocene/src/App.jsx
+++ audiocene/src/App.jsx
@@ -9,28 +9,31 @@ import RecordingsList from "./components/RecordingsList";
 import Recording from "./components/Recording";
 import { RecordingsProvider } from "./contexts/RecordingsContext";
 import Form from "./components/Form";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <RecordingsProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index element={<Homepage />}></Route>
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<RecordingsList />} />
-            <Route
-              path="explore"
-              element={<p>Find a recording on the map...</p>}
-            />
-            <Route path="favourites/:id" element={<Recording />} />
-            <Route path="add" element={<Form />} />
-            <Route path="favourites" element={<RecordingsList />} />
-          </Route>
-          <Route path="login" element={<Login />}></Route>
-          <Route path="about" element={<About />}></Route>
-        </Routes>
-      </BrowserRouter>
-    </RecordingsProvider>
+    <AuthProvider>
+      <RecordingsProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<Homepage />}></Route>
+            <Route path="app" element={<AppLayout />}>
+              <Route index element={<RecordingsList />} />
+              <Route
+                path="explore"
+                element={<p>Find a recording on the map...</p>}
+              />
+              <Route path="favourites/:id" element={<Recording />} />
+              <Route path="add" element={<Form />} />
+              <Route path="favourites" element={<RecordingsList />} />
+            </Route>
+            <Route path="login" element={<Login />}></Route>
+            <Route path="about" element={<About />}></Route>
+          </Routes>
+        </BrowserRouter>
+      </RecordingsProvider>
+    </AuthProvider>
   );
 }
 
---
+++ audiocene/src/components/Login.jsx
@@ -0,0 +1,53 @@
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useEffect, useState } from "react";
+
+export default function Login() {
+  // PRE-FILL FOR DEV PURPOSES
+  const [email, setEmail] = useState("jack@example.com");
+  const [password, setPassword] = useState("qwerty");
+
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (email && password) login(email, password);
+  }
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app", { replace: true });
+    },
+    [isAuthenticated, navigate]
+  );
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="email">Email address</label>
+        <input
+          type="email"
+          id="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </div>
+
+      <div>
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </div>
+
+      <div>
+        <Button>Login</Button>
+      </div>
+    </form>
+  );
+}
