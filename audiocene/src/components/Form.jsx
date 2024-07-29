848aab67dc417908343a01f604887addf4e8f744
Andy Soloman <andyjsoloman@gmail.com>
2024-07-29 16:40:25 -0700

    add react hook form begin create form edit

--- audiocene/src/components/Form.jsx
+++ audiocene/src/components/Form.jsx
@@ -1,3 +1,5 @@
+import { Controller, useForm } from "react-hook-form";
+
 import useUrlPositon from "../hooks/useUrlPosition";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
@@ -11,97 +13,84 @@ import { useNavigate } from "react-router-dom";
 const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 export default function Form() {
+  const { register, handleSubmit, control, setValue } = useForm();
+
   const { createRecording, isLoading } = useRecordings();
   const navigate = useNavigate();
   const [lat, lng] = useUrlPositon();
-  const [title, setTitle] = useState("");
   const [locality, setLocality] = useState("");
   const [country, setCountry] = useState("");
-  const [date, setDate] = useState(new Date());
-  const [notes, setNotes] = useState("");
-
   const [isLoadingGeocoding, setIsLoadingGeoCoding] = useState(false);
   const [geocodingError, setGeocodingError] = useState("");
 
-  useEffect(
-    function () {
-      if (!lat && !lng) return;
-      async function fetchRecordingData() {
-        try {
-          setIsLoadingGeoCoding(true);
-          const res = await fetch(
-            `${BASE_URL}?latitude=${lat}&longitude=${lng}`
-          );
-          const data = await res.json();
+  useEffect(() => {
+    if (!lat && !lng) return;
 
-          setLocality(data.locality || "");
-          setCountry(data.countryName || "");
-        } catch (err) {
-          setGeocodingError(err.message);
-        } finally {
-          setIsLoadingGeoCoding(false);
-        }
+    async function fetchRecordingData() {
+      try {
+        setIsLoadingGeoCoding(true);
+        const res = await fetch(`${BASE_URL}?latitude=${lat}&longitude=${lng}`);
+        const data = await res.json();
+
+        setLocality(data.locality || "");
+        setCountry(data.countryName || "");
+        setValue("locality", data.locality || "");
+        setValue("country", data.countryName || "");
+      } catch (err) {
+        setGeocodingError(err.message);
+      } finally {
+        setIsLoadingGeoCoding(false);
       }
-      fetchRecordingData();
-    },
-    [lat, lng]
-  );
+    }
 
-  async function handleSubmit(e) {
-    e.preventDefault();
-    if (!date) return;
-    const newRecording = {
-      title,
-      locality,
-      country,
-      date,
-      description: notes,
-      position: { lat, lng },
-    };
-    await createRecording(newRecording);
-    navigate("/app/explore");
+    fetchRecordingData();
+  }, [lat, lng, setValue]);
+
+  function onSubmit(data) {
+    console.log(data);
+    // navigate("/app/explore");
+    createRecording(data);
   }
 
   if (isLoadingGeocoding) return <LoadingSpinner />;
-
-  if (!lat && !lng) return <p>Start by clicking somewhere on the map </p>;
-
+  if (!lat && !lng) return <p>Start by clicking somewhere on the map</p>;
   if (geocodingError) return <Message message={geocodingError} />;
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <label htmlFor="title">Recording Title</label>
-        <input
-          id="title"
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-        />
+        <input id="title" defaultValue="" {...register("title")} />
       </div>
       <div>
-        <label htmlFor="locality">Locality</label>
-        <input
-          id="recordingTitle"
-          onChange={(e) => setLocality(e.target.value)}
-          value={locality}
-        />
+        <label htmlFor="locality">Locality:</label>
+        <p>{locality}</p>
+        <input id="locality" type="hidden" {...register("locality")} />
       </div>
       <div>
-        <label htmlFor="recordingDate">Date of Recording</label>
-        <DatePicker
-          id="date"
-          onChange={(date) => setDate(date)}
-          selected={date}
-          dateFormat="dd/MM/yyyy"
+        <label htmlFor="country">Country:</label>
+        <p>{country}</p>
+        <input id="country" type="hidden" {...register("country")} />
+      </div>
+      <div>
+        <label htmlFor="date">Date of Recording</label>
+        <Controller
+          control={control}
+          name="date"
+          defaultValue={new Date()}
+          render={({ field }) => (
+            <DatePicker
+              id="date"
+              selected={field.value}
+              onChange={field.onChange}
+              dateFormat="dd/MM/yyyy"
+            />
+          )}
         />
       </div>
       <div>
         <label htmlFor="notes">Notes</label>
-        <textarea
-          id="notes"
-          onChange={(e) => setNotes(e.target.value)}
-          value={notes}
-        />
+        <textarea id="notes" defaultValue="" {...register("notes")} />
       </div>
       <Button>Add Recording</Button>
     </form>
