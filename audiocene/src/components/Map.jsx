5185920c8ab22c51901e0569ff8f9802781b1f1e
Andy Soloman <andyjsoloman@gmail.com>
2024-08-13 20:00:50 -0700

    eslint updates and error fixing

--- audiocene/src/components/Map.jsx
+++ audiocene/src/components/Map.jsx
@@ -5,6 +5,7 @@ import {
   Marker,
   Popup,
   useMapEvents,
+  L,
 } from "react-leaflet";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styled from "styled-components";
