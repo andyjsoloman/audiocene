ab94464c7549006e4818d3195059ef31dea4733b
Andy Soloman <andyjsoloman@gmail.com>
2025-03-31 19:14:21 -0700

    edit supabase.js

--- audiocene/src/services/supabase.js
+++ audiocene/src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://naiqpffpxlusflmdzeqa.supabase.co";
-export const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5haXFwZmZweGx1c2ZsbWR6ZXFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4MzA0NzAsImV4cCI6MjAzMTQwNjQ3MH0.Z8zlATYa5yqbHGcdT3NcbnOfxb6w3EdkH1mpkGX7110";
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
